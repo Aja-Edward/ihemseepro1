249f29ad1537cd4517d67f271722595821c6c520
Aja-Edward <77103145+Aja-Edward@users.noreply.github.com>
2023-11-02 12:44:54 +0100

    contact update

--- app/registry.js
+++ app/registry.js
@@ -5,8 +5,7 @@ import { useServerInsertedHTML } from 'next/navigation'
 import { StyleRegistry, createStyleRegistry } from 'styled-jsx'
 
 export default function StyledJsxRegistry({ children }) {
-  // Only create stylesheet once with lazy initial state
-  // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+ 
   const [jsxStyleRegistry] = useState(() => createStyleRegistry())
 
   useServerInsertedHTML(() => {
