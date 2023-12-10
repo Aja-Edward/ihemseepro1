647f0197879a5251dddd9abc7ae1751d3e69b3b1
Aja-Edward <77103145+Aja-Edward@users.noreply.github.com>
2023-12-10 22:41:09 +0100

    Your commit message here

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,7 @@ import Navbar from '@components/Navbar'
 import Footer from '@components/Footer'
 
 const roboto = Roboto({
-  weights: ['100', '300', '400', '500', '700', '900'],
+  weight: ['100', '300', '400', '500', '700', '900'],
   subsets: ['latin'],
 })
 
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,11 +1,13 @@
 import { Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@components/Valuecard.css'
+import Image from 'next/image'
 
 export const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <Col size={12} sm={6} md={4}>
       <div className='proj-imgbx'>
-        <img src={imgUrl} />
+        <Image width={300} height={300} src={imgUrl} alt='our luxury system' />
         <div className='proj-txtx'>
           <h4>{title}</h4>
           <span>{description}</span>
